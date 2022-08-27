45219743d82fa5083b13e82d92055f0fd89581f4
minju moon <mmj9908@gmail.com>
2022-08-27 17:18:49 +0900

    :sparkles: 투두리스트 삭제기능
    투두리스트 삭제기능

--- src/App.tsx
+++ src/App.tsx
@@ -23,31 +23,44 @@ const App: FC = () => {
     setDeadline(0);
   };
 
+  const completeTask = (taskNameToDelete: string): void => {
+    setTodo(
+      todo.filter((task) => {
+        return task.taskName !== taskNameToDelete; //  완료한 일은 삭제
+      })
+    );
+  };
+
   return (
     <div className="App">
-      <div className="header">
-        <div className="inputContainer">
-          <input
-            type="text"
-            placeholder="Task..."
-            name="task"
-            value={task}
-            onChange={handleChange}
-          />
-          <input
-            type="number"
-            placeholder="Deadline(in Days)..."
-            name="deadline"
-            value={deadline}
-            onChange={handleChange}
-          />
+      <h1 className="heading">ToDoList</h1>
+      <div className="wrapper">
+        <div className="header">
+          <div className="inputContainer">
+            <input
+              type="text"
+              placeholder="Task..."
+              name="task"
+              value={task}
+              onChange={handleChange}
+            />
+            <input
+              type="number"
+              placeholder="Deadline(in Days)..."
+              name="deadline"
+              value={deadline}
+              onChange={handleChange}
+            />
+          </div>
+          <button onClick={addTask}> Add Task </button>
         </div>
-        <button onClick={addTask}> Add Task </button>
-      </div>
-      <div className="todoList">
-        {todo.map((task: ITask, key: number) => {
-          return <TodoTask key={key} task={task} />;
-        })}
+        <div className="todoList">
+          {todo.map((task: ITask, key: number) => {
+            return (
+              <TodoTask key={key} task={task} completeTask={completeTask} />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
