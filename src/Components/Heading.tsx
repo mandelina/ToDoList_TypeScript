739ff6d54e051ece75f36c3d9d4cedcfefc46c04
minju moon <mmj9908@gmail.com>
2022-09-26 22:53:53 +0900

    날짜 오류 수정

--- src/Components/Heading.tsx
+++ src/Components/Heading.tsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const Heading = () => {
   //월
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = ("0" + (date.getMonth() + 1)).slice(-2);
-  const day = ("0" + date.getDay()).slice(-2);
-  const dateStr = year + "년 " + month + "월 " + day + "일";
+  const today = new Date();
+  let year = today.getFullYear(); // 년도
+  let month = today.getMonth() + 1; // 월
+  let date = today.getDate(); // 날짜
+  const dateStr = year + "년 " + month + "월 " + date + "일";
 
   // 시간
-  const hours = ("0" + date.getHours()).slice(-2);
-  const minutes = ("0" + date.getMinutes()).slice(-2);
+  const hours = ("0" + today.getHours()).slice(-2);
+  const minutes = ("0" + today.getMinutes()).slice(-2);
   const timeStr = hours + ":" + minutes;
 
   return (
